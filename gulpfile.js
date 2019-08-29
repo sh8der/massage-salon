1584250740e6e210a0ce81bf9d67a77a74589d50
Sh8der <youxenux@gmail.com>
2019-08-29 16:59:48 +0600

    start

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,21 @@
-/**
- * A simple Gulp 4 Starter Kit for modern web development.
- *
- * @package @jr-cologne/create-gulp-starter-kit
- * @author JR Cologne <kontakt@jr-cologne.de>
- * @copyright 2019 JR Cologne
- * @license https://github.com/jr-cologne/gulp-starter-kit/blob/master/LICENSE MIT
- * @version v0.10.10-beta
- * @link https://github.com/jr-cologne/gulp-starter-kit GitHub Repository
- * @link https://www.npmjs.com/package/@jr-cologne/create-gulp-starter-kit npm package site
- *
- * ________________________________________________________________________________
- *
- * gulpfile.js
- *
- * The gulp configuration file.
- *
- */
+const gulp = require('gulp');
+const del = require('del');
+const sourcemaps = require('gulp-sourcemaps');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const minifyCss = require('gulp-clean-css');
+const babel = require('gulp-babel');
+const webpack = require('webpack-stream');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
-const gulp = require('gulp'),
-	del = require('del'),
-	sourcemaps = require('gulp-sourcemaps'),
-	plumber = require('gulp-plumber'),
-	sass = require('gulp-sass'),
-	less = require('gulp-less'),
-	stylus = require('gulp-stylus'),
-	autoprefixer = require('gulp-autoprefixer'),
-	minifyCss = require('gulp-clean-css'),
-	babel = require('gulp-babel'),
-	webpack = require('webpack-stream'),
-	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
-	imagemin = require('gulp-imagemin'),
-	browserSync = require('browser-sync').create(),
-	pug = require('gulp-pug'),
-
-	src_folder = './src/',
-	src_assets_folder = src_folder + 'assets/',
-	dist_folder = './dist/',
-	dist_assets_folder = dist_folder + 'assets/',
-	node_modules_folder = './node_modules/',
-	dist_node_modules_folder = dist_folder + 'node_modules/',
-
-	node_dependencies = Object.keys(require('./package.json').dependencies || {});
+let src_folder = './src/';
+let src_assets_folder = src_folder + 'assets/';
+let dist_folder = './dist/';
+let dist_assets_folder = dist_folder + 'assets/';
 
 gulp.task('clear', () => del([dist_folder]));
 
@@ -50,17 +24,6 @@ gulp.task('html', () => {
 			base: src_folder,
 			since: gulp.lastRun('html')
 		})
-		.pipe(gulp.dest(dist_folder))
-		.pipe(browserSync.stream());
-});
-
-gulp.task('pug', () => {
-	return gulp.src([src_folder + 'pug/**/!(_)*.pug'], {
-			base: src_folder + 'pug',
-			since: gulp.lastRun('pug')
-		})
-		.pipe(plumber())
-		.pipe(pug())
 		.pipe(gulp.dest(dist_folder))
 		.pipe(browserSync.stream());
 });
@@ -86,36 +49,9 @@ gulp.task('font', () => {
 	return gulp.src(src_assets_folder + 'font/**/*', {
 			since: gulp.lastRun('font')
 		})
-		.pipe(gulp.dest(dist_assets_folder + 'font'));
+		.pipe(gulp.dest(dist_assets_folder + 'font'))
+		.pipe(browserSync.stream());
 })
-
-gulp.task('less', () => {
-	return gulp.src([src_assets_folder + 'less/**/!(_)*.less'], {
-			since: gulp.lastRun('less')
-		})
-		.pipe(sourcemaps.init())
-		.pipe(plumber())
-		.pipe(less())
-		.pipe(autoprefixer())
-		.pipe(minifyCss())
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(dist_assets_folder + 'css'))
-		.pipe(browserSync.stream());
-});
-
-gulp.task('stylus', () => {
-	return gulp.src([src_assets_folder + 'stylus/**/!(_)*.styl'], {
-			since: gulp.lastRun('stylus')
-		})
-		.pipe(sourcemaps.init())
-		.pipe(plumber())
-		.pipe(stylus())
-		.pipe(autoprefixer())
-		.pipe(minifyCss())
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(dist_assets_folder + 'css'))
-		.pipe(browserSync.stream());
-});
 
 gulp.task('js', () => {
 	return gulp.src([src_assets_folder + 'js/**/*.js'], {
@@ -146,23 +82,7 @@ gulp.task('images', () => {
 		.pipe(browserSync.stream());
 });
 
-gulp.task('vendor', () => {
-	if (node_dependencies.length === 0) {
-		return new Promise((resolve) => {
-			console.log("No dependencies specified");
-			resolve();
-		});
-	}
-
-	return gulp.src(node_dependencies.map(dependency => node_modules_folder + dependency + '/**/*.*'), {
-			base: node_modules_folder,
-			since: gulp.lastRun('vendor')
-		})
-		.pipe(gulp.dest(dist_node_modules_folder))
-		.pipe(browserSync.stream());
-});
-
-gulp.task('build', gulp.series('clear', 'html', 'sass', 'js', 'images', 'font' /* , 'vendor' */ ));
+gulp.task('build', gulp.series('clear', 'html', 'sass', 'js', 'images', 'font'));
 
 gulp.task('dev', gulp.series('html', 'sass', 'js'));
 
@@ -182,12 +102,6 @@ gulp.task('watch', () => {
 		src_assets_folder + 'images/**/*.+(png|jpg|jpeg|gif|svg|ico)'
 	];
 
-	const watchVendor = [];
-
-	node_dependencies.forEach(dependency => {
-		watchVendor.push(node_modules_folder + dependency + '/**/*.*');
-	});
-
 	const watch = [
 		src_folder + '**/*.html',
 		src_assets_folder + 'sass/**/*.sass',
@@ -197,7 +111,6 @@ gulp.task('watch', () => {
 
 	gulp.watch(watch, gulp.series('dev')).on('change', browserSync.reload);
 	gulp.watch(watchImages, gulp.series('images')).on('change', browserSync.reload);
-	// gulp.watch(watchVendor, gulp.series('vendor')).on('change', browserSync.reload);
 });
 
 gulp.task('default', gulp.series('build', gulp.parallel('serve', 'watch')));
