6a45f40fea70a43c6d3c0d9b95ba0dc35ad135fb
Кирилл <sh8der@mail.ru>
2019-09-08 02:01:16 +0600

    start adaptive

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,40 @@
 import modernizr from './libs/modernizr-custom.js'
-import masonry from 'masonry-layout'
-import baguetteBox from 'baguettebox.js';
+import Masonry from 'masonry-layout'
+import baguetteBox from 'baguettebox.js'
+import anime from 'animejs/lib/anime.es.js'
+import imagesLoaded from 'imagesloaded'
 
+// инициализируем открытие фоток в модельном окне
+baguetteBox.run('.photo-block__items')
+
+// проверяем загрузились ли картинки для галлереии, если да то строим сетку масонри и инимируем
+if (document.querySelector('.photo-block__items') !== null) {
+
+	imagesLoaded('.photo-block__items', () => {
+
+		let photoGalleryItemsWrapper = document.querySelector('.photo-block__items')
+
+		const msnry = new Masonry(photoGalleryItemsWrapper, {
+			// options
+			itemSelector: '.photo-block__item',
+			columnWidth: '.photo-block__item',
+			gutter: 10,
+			fitWidth: true
+		});
+
+		anime({
+			targets: '.photo-block__item',
+			duration: 500,
+			opacity: 1,
+			direction: 'normal',
+			loop: false,
+			easing: 'easeInOutQuad',
+			delay: function (el, i, l) {
+				return i * 50;
+			}
+		});
+
+
+	})
+
+}
